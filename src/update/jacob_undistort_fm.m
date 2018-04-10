151213d863f13ce897c9fbccf86f8c8cc339b836
ayush1405 <36578929+ayush1405@users.noreply.github.com>
2018-04-10 18:39:08 -0400

    Add files via upload

---
+++ src/update/jacob_undistort_fm.m
@@ -0,0 +1,21 @@
+function J_undistor = jacob_undistort_fm(cam,uvd)
+
+Cx=cam.Cx; Cy=cam.Cy;
+k1=cam.k1; k2=cam.k2;
+dx=cam.dx; dy=cam.dy;
+
+ud=uvd(1); vd=uvd(2);
+
+xd=(uvd(1)-Cx)*dx;
+yd=(uvd(2)-Cy)*dy;
+  
+rd2=xd*xd+yd*yd;
+rd4=rd2*rd2;
+     
+uu_ud=(1+k1*rd2+k2*rd4)+(ud-Cx)*(k1+2*k2*rd2)*(2*(ud-Cx)*dx*dx);
+vu_vd=(1+k1*rd2+k2*rd4)+(vd-Cy)*(k1+2*k2*rd2)*(2*(vd-Cy)*dy*dy);
+    
+uu_vd=(ud-Cx)*(k1+2*k2*rd2)*(2*(vd-Cy)*dy*dy);
+vu_ud=(vd-Cy)*(k1+2*k2*rd2)*(2*(ud-Cx)*dx*dx);
+     
+J_undistor=[uu_ud uu_vd;vu_ud vu_vd];
