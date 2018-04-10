f5c2871ba1ef48cb8431d88aa7c125cdd9ab8a6e
Manuel Soto Santiago <sotosant@umich.edu>
2018-04-10 16:47:41 -0400

    map management and deleting of landmarks that are inconsistent

---
+++ src/delete_landmarks.m
@@ -0,0 +1,51 @@
+function delete_landmarks(idx)
+% deletes landmarks from state vector that are inconsistent
+% input: index of landmarks in full state vector that will be deleted
+
+global State;
+
+dimX = State.Ekf.dimR;
+dimL = State.Ekf.dimL;
+
+% if index vector is not empty then remove index from state vector
+while ~isempty(idx)
+    i = idx(1);
+    iL = State.Ekf.iL{i}; % index of landmark that will be removed
+    
+    State.Ekf.mu(iL) = []; % removes landmark from state vector
+    State.Ekf.Sigma(iL,:) = []; % removes part of landmark in covariance
+    State.Ekf.Sigma(:,iL) = []; % removes the remaining part of landmark in covariance
+    
+    for j = i+1:State.Ekf.nL
+        State.Ekf.iL{j} = State.Ekf.iL{j} - dimL; % correct indexing of remaining landmarks
+    end
+    State.Ekf.iM(iL(end)+1:end) = State.Ekf.iM(iL(end)+1:end) - dimL; % shift landmarks in map oe position over
+    
+    % Remove landmark from all other State parameters
+    State.Ekf.iM(i) = [];
+    State.Ekf.iL(i) = [];
+    State.Ekf.sL(i) = [];
+    State.Ekf.z(i) = [];
+    State.Ekf.h(i) = [];
+    State.Ekf.H(i) = [];
+    State.Ekf.S(i) = [];
+    State.Ekf.Q(i) = [];
+    State.Ekf.status(i) = [];
+    State.Ekf.matched(i) = [];
+    State.Ekf.match_attempts(i) = [];
+    State.Ekf.individually_compatible(i) = []; 
+    State.Ekf.init_t(i) = [];
+    State.Ekf.init_z(i) = [];
+    State.Ekf.init_x(i) = [];
+    State.Ekf.patch_matching(i) = [];
+    
+    % Update landmark count
+    State.Ekf.nL = State.Ekf.nL - 1;
+    
+    % Update indexes after removing the one landmark
+    idx(1) = [];
+    idx = idx-1;
+
+end
+
+
---
+++ src/map_management.m
@@ -0,0 +1,41 @@
+function map_management()
+% check the total number of landmarks and
+% detect new landmarks from the image, or
+% delete inappropriate landmarks from the map
+
+global State;
+global Param;
+
+% delete landmarks that have not been matched 
+% or re-detected in multiple frames therefore we iterate
+% through every landmark in full state vector
+
+delete_idx = [];
+for i = 1:State.Ekf.nL
+    iL = State.Ekf.iL{i}; % current landmark index
+    mi = State.Ekf.mu(iL); % current landmark state
+    
+    % ratio of current landmark associations over attemps to make
+    % association. if the ratio is small, the landmark is interpreted as
+    % not being consistent and is deleted
+    ratio = State.Ekf.matched(i)/State.Ekf.match_attempts(i); 
+    if (State.Ekf.match_attempts(i) > 10 && ratio < 0.5)
+        delete_idx = [delete_idx, i];
+    end
+
+end
+delete_landmarks(delete_idx);
+measured = sum(State.Ekf.individually_compatible);
+
+% detect new landmarks if total number of landmarks
+% is less than threshold
+
+if State.Ekf.nL == 0 % case when initializing    
+    detect_corners(Param.map.min_nL);    
+else
+    if measured < Param.map.min_nL % detect more landmarks if under threshold
+        detect_corners(Param.map.min_nL-measured);
+    end
+end
+
+update_landmarks_info();
