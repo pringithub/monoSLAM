55a046e9e132d79c94151ef9399c8639ce7f9d19
Phillip Ring <pring@umich.edu>
2018-04-10 18:32:25 -0400

    sorted files into folders

--- src/map_management/map_management.m
+++ src/map_management/map_management.m
@@ -10,7 +10,7 @@ global Param;
 % or re-detected in multiple frames therefore we iterate
 % through every landmark in full state vector
 
-delete_idx = [];
+rem_li_idx = [];
 for i = 1:State.Ekf.nL
     iL = State.Ekf.iL{i}; % current landmark index
     mi = State.Ekf.mu(iL); % current landmark state
@@ -20,22 +20,22 @@ for i = 1:State.Ekf.nL
     % not being consistent and is deleted
     ratio = State.Ekf.matched(i)/State.Ekf.match_attempts(i); 
     if (State.Ekf.match_attempts(i) > 10 && ratio < 0.5)
-        delete_idx = [delete_idx, i];
+        rem_li_idx = [rem_li_idx, i];
     end
 
 end
-delete_landmarks(delete_idx);
+remove_landmarks(rem_li_idx);
 measured = sum(State.Ekf.individually_compatible);
 
 % detect new landmarks if total number of landmarks
 % is less than threshold
 
 if State.Ekf.nL == 0 % case when initializing    
-    detect_corners(Param.map.min_nL);    
+    corner_detection(Param.map.min_nL);    
 else
     if measured < Param.map.min_nL % detect more landmarks if under threshold
-        detect_corners(Param.map.min_nL-measured);
+        corner_detection(Param.map.min_nL-measured);
     end
 end
 
-update_landmarks_info();
+update_landmarks_attributes();
