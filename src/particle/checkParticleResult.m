6aee84594e7b3957d512693baac7c256563cd886
Alex Wang <muyang@umich.edu>
2018-04-10 22:41:31 -0400

    add checkParticleResult

---
+++ src/particle/checkParticleResult.m
@@ -0,0 +1,33 @@
+%% Compute each available feature's standard deviation, and convert the feauture with best-estimated depth distribution to a standard Gaussian one. If no features are good enough, return NaN.
+
+function checkParticleResult()
+    global State;
+    global Param;
+    
+    stdv = zeros(State.Ekf.nL, 1);
+    
+    % set a threshold here.
+    thres = 0.5;
+    
+    State.P.rhoMean = zeros(State.Ekf.nL,1);
+    State.P.rhoVar = zeros(State.Ekf.nL,1);
+    
+    for i = 1:1:State.Ekf.nL
+        stdv(i) = std(State.P.featureProbMatrix(i,:));
+        
+        % Not used in Ekf Update
+        if stdv(i) > thres
+            State.Ekf.individually_compatible(i) = 0;
+        end
+        
+        for j = 1:100
+            State.P.rhoMean(i) = State.P.rhoMean(i) + State.P.featureInverseDepth(i, j) * State.P.featureProbMatrix(i, j);
+        end
+        
+        for j = 1:100
+            emex = State.P.featureInverseDepth(bestIndex, i) - rho;
+           State.P.rhoVar(i) = State.P.rhoVar(i) + emex*emex' *  State.P.featureProbMatrix(i, j);
+        end
+    end
+   
+end
