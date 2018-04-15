4f9228bb58f7669cd188d17f7d79604237364a03
Phillip Ring <pring@umich.edu>
2018-04-15 17:54:36 -0400

    added video->dirOfImgs script

---
+++ create_dir_from_vid.m
@@ -0,0 +1,19 @@
+
+
+function create_dir_from_vid(video_filename,save_folder)
+
+v = VideoReader(video_filename);
+
+i = 0;
+while hasFrame(v)
+    img = rgb2gray( readFrame(v) );
+    img = img(1:3:end,1:3:end);
+    
+    img_filename = sprintf('%s/img%010d.jpg',save_folder,i);
+    imwrite(img, img_filename, 'jpg');
+    i = i+1;
+end
+
+
+
+end
